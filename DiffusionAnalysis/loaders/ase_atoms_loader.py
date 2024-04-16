25f0bf4fb506c5718eb10b94da7e081c03d7826a
Chris Davies <christophertdavies@gmail.com>
2024-04-16 12:31:36 +0100

    Add temperature and the ability to change atom types when loading.

--- DiffusionAnalysis/loaders/ase_atoms_loader.py
+++ DiffusionAnalysis/loaders/ase_atoms_loader.py
@@ -18,7 +18,10 @@ class ASEListStructureLoader(StructureLoader):
         None means load all structures.
         NOT RECOMMENDED TO USE THIS ARGUMENT FOR THIS LOADER
         (Just pass the slice to the list directly before passing it to this loader)
-    md_timestep : Optional[float], default=1
+    md_temperature : temperature: Union[float, List[float]] = 300
+        default=300 k. A float or list of floats representing the temperature(s) of the MD simulation. If a list
+        is provided, the length should match the number of steps in the trajectory.
+    md_timestep : float, default=1
         The time step of the MD simulation, representing the time difference between consecutive frames.
     md_time_unit : Union[str, TimeUnit], default='ps'
         The unit of time for the `md_timestep`. Accepted string values are 'fs' (femtoseconds),
@@ -27,23 +30,28 @@ class ASEListStructureLoader(StructureLoader):
     md_start_offset : Optional[float], default=None
         The starting time offset of the trajectory in the specified time unit. If `None`, the starting time
         will be determined based on the `structures_slice`.
+    atom_map : Optional[dict], default=None
+        A dictionary to change the atom types in the trajectory. The dictionary should have the format
+        `{old_atom_type: new_atom_type}`. If `None`, the atom types will not be changed.
     '''
 
     def __init__(self,
                  atoms_list: List[Atoms],
                  structures_slice: Optional[Union[slice, int]] = None,
-                 md_timestep: Optional[float] = 1,
+                 md_temperature: Optional[Union[float, List[float]]] = None,
+                 md_timestep: float = 1,
                  md_time_unit: Union[str, TimeUnit] = 'ps',
-                 md_start_offset: Optional[float] = None):
+                 md_start_offset: Optional[float] = None,
+                 atom_map: Optional[dict] = None):
         '''
         Initialize the ASEListStructureLoader.
         '''
         self.atoms_list = atoms_list
-        super().__init__('', structures_slice, md_timestep, md_time_unit, md_start_offset)
+        super().__init__('', structures_slice, md_temperature ,md_timestep, md_time_unit, md_start_offset, atom_map)
 
         if self.structures_slice is None:
             self.atoms_list = atoms_list
-        elif isinstance(self.structures_slice, slice):
+        elif isinstance(self.structures_slice, slice): 
             self.atoms_list = atoms_list[self.structures_slice]
         else:
             self.atoms_list = [atoms_list[self.structures_slice]]
@@ -87,3 +95,6 @@ class ASEListStructureLoader(StructureLoader):
             A tuple containing the start time, end time, timestep, and time unit of the trajectory.
         """
         return super().get_trajectory_time_info
+    
+    def get_temperature(self, index: int) -> float:
+        return super().get_temperature(index)
